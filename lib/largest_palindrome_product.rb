4087c3eca1cb9c0f2e3104cefe5ad33da7be7a28
mdeskin <michaelhaydendeskin@gmail.com>
2015-11-08 04:43:08 +0000

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,37 @@
-# Implement your procedural solution here!
+def largest_palindrome_product()
+  palindromes = []
+  first = 999
+  second = 999
+
+  until first == 99
+    until second == 99
+          product = first * second
+          product_string = product.to_s
+          array = product_string.split ""
+ #          puts array
+          array_size = array.size
+          if array_size == 6
+            if array[0] == array[5] && array[1] == array[4] && array[2] == array[3]
+              palindromes << product
+#               puts "if"
+              # break
+            end
+          elsif array_size == 5
+            if array[0] == array[4] && array[2] == array[3]
+              palindromes << product
+  #             puts "elsif"
+                # break
+            end
+          else
+            puts "else"
+          end
+
+    second -= 1
+    end
+    second = 999
+    first -= 1
+    end
+puts "#{first} #{second}"
+puts palindromes.max
+palindromes.max
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,40 @@
-# Implement your object-oriented solution here!
+class LargestPalindromeProduct
+
+#   def initialize(answer)
+  #   @answer = answer
+  # end
+
+  def answer
+    palindromes = []
+    first = 999
+    second = 999
+
+    until first == 99
+      until second == 99
+            product = first * second
+            product_string = product.to_s
+            array = product_string.split ""
+            array_size = array.size
+            if array_size == 6
+              if array[0] == array[5] && array[1] == array[4] && array[2] == array[3]
+                palindromes << product
+              end
+          elsif array_size == 5
+            if array[0] == array[4] && array[2] == array[3]
+              palindromes << product
+            end
+          else
+            puts "else"
+          end
+    second -= 1
+    end
+    second = 999
+    first -= 1
+    end
+puts "#{first} #{second}"
+puts palindromes.max
+palindromes.max
+end
+
+
+end
